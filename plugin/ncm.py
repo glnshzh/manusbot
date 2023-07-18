a48f3b1e05178765de4ad344e850c4e4cbb8df3e
glnshzh <478184350@qq.com>
2023-07-19 00:44:18 +0800

    music

--- plugin/ncm.py
+++ plugin/ncm.py
@@ -1,67 +1,64 @@
-from typing import Tuple, Any, Union
-import nonebot
-from .data_source import nncm, ncm_config, setting, Q, cmd
+import time
+from nonebot import on_keyword, Bot
+from nonebot.adapters.onebot.v11 import Event, MessageEvent, bot, MessageSegment
+from nonebot.internal.params import ArgPlainText
+from nonebot.typing import T_State
+from pyncm.apis.cloudsearch import GetSearchResult
+from pyncm.apis.login import LoginViaCellphone
+from pyncm.apis.track import GetTrackAudio
+from pyncm.apis.login import LoginViaAnonymousAccount
 
-__plugin_meta__ = nonebot.plugin.PluginMetadata(
-    name='网易云无损音乐下载',
-    description='✨ 基于go-cqhttp与nonebot2的 网易云 无损音乐下载 ✨',
-    usage=(
-        '将网易云歌曲/歌单分享到群聊即可自动解析\n'
-        '回复分享消息 + 文字`下载` 即可开始下载歌曲并上传到群文件(需要稍等一会)\n'
-        '指令：\n'
-        f'开启下载：{cmd}ncm t\n'
-        f'关闭下载：{cmd}ncm f\n'
-        f'点歌：{cmd}点歌 歌名'
-    ),
-    extra={'version': '1.5.0'}
-)
+LoginViaCellphone("18932232420","2028321627htc")
+music=on_keyword(["music"])
+option_text='''请选择想要的功能:
+              1.转换成音频发送
+              2.下载原曲
+              3.网页快捷听歌(推荐)
+'''
 
-from nonebot.adapters.onebot.v11 import GroupMessageEvent, PrivateMessageEvent
 
-TRUE = ["True", "T", "true", "t"]
-FALSE = ["False", "F", "false", "f"]
-ADMIN = ["owner", "admin", "member"]
+@music.got("song_name",prompt="输入想要的歌曲米喵~")
+async def got_song_name(state:T_State,song_name: str = ArgPlainText()):
+    print(song_name)
+    songs = GetSearchResult(song_name)['result']['songs']
+    print(songs)
+    for i in range(10):
+        song = songs[i]
+        state[str(i)]=song
+        song_info = str(i) + ". " + "歌曲名：" + song['name']
+        if len(song['alia']) > 0:
+            song_info += ",介绍:" + song['alia'][0]
+        await music.send(song_info)
+        time.sleep(1)
 
-# ===============Rule=============
-async def song_is_open(event: Union[GroupMessageEvent, PrivateMessageEvent]) -> bool:
-    if isinstance(event, GroupMessageEvent):
-        info = setting.search(Q["group_id"] == event.group_id)
-        if info:
-            return info[0]["song"]
+@music.got("song_id",prompt="请输入想要的歌曲编号喵~")
+async def get_music_id(state:T_State,song_id: str = ArgPlainText()):
+    state["song_id"]=song_id
+
+
+@music.got("option",prompt=option_text)
+async def _(state:T_State,option:str=ArgPlainText()):
+    print(option)
+    print(state.get(state.get('song_id')))
+    song = state.get(state.get('song_id'))
+    # for key in song:
+    #     print(key)
+    pic=song['al']['picUrl']
+    pic=MessageSegment.image(pic)
+    await music.send(pic)
+    if option=="3":
+        download=GetTrackAudio(song['id'],bitrate=6400 * 2000)
+        url=download['data'][0]['url']
+        print(url)
+        if url=='' or url==None:
+            await music.send("歌曲收听,网易云没有上架该音乐")
         else:
-            setting.insert({"group_id": event.group_id, "song": False, "list": False})
-            return False
-    elif isinstance(event, PrivateMessageEvent):
-        info = setting.search(Q["user_id"] == event.user_id)
-        if info:
-            return info[0]["song"]
-        else:
-            setting.insert({"user_id": event.user_id, "song": True, "list": True})
-            return True
+            await music.send(url)
+    if option=="2":
+        print("")
+    await music.finish()
 
 
-async def playlist_is_open(event: Union[GroupMessageEvent, PrivateMessageEvent]) -> bool:
-    if isinstance(event, GroupMessageEvent):
-        info = setting.search(Q["group_id"] == event.group_id)
-        if info:
-            return info[0]["list"]
-        else:
-            setting.insert({"group_id": event.group_id, "song": False, "list": False})
-            return False
-    elif isinstance(event, PrivateMessageEvent):
-        info = setting.search(Q["user_id"] == event.user_id)
-        if info:
-            return info[0]["list"]
-        else:
-            setting.insert({"user_id": event.user_id, "song": True, "list": True})
-            return True
-async def check_search() -> bool:
-    info = setting.search(Q["global"] == "search")
-    if info:
-        return info[0]["value"]
-    else:
-        setting.insert({"global": "search", "value": True})
-        return True
 
 
 
